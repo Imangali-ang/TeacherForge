026f4d9af047d50c152c00bbbf6b41014438b177
imangali <i.kaliakhmetov@strongte.am>
2024-01-21 19:45:46 +0500

    exception handling and properties

--- src/main/java/kz/teacher/forge/teacherforge/TeacherForgeApplication.java
+++ src/main/java/kz/teacher/forge/teacherforge/TeacherForgeApplication.java
@@ -1,13 +1,24 @@
 package kz.teacher.forge.teacherforge;
 
+import kz.teacher.forge.teacherforge.exception.ApiExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TeacherForgeApplication {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     public static void main(String[] args) {
         SpringApplication.run(TeacherForgeApplication.class, args);
+    }
+
+    @Bean
+    public ApiExceptionHandler exceptionHandler() {
+        return new ApiExceptionHandler(applicationName);
     }
 
 }
