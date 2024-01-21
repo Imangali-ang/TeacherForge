d291aee28877eef7c3215eca924d34fd4919fa14
imangali <i.kaliakhmetov@strongte.am>
2024-01-21 19:03:22 +0500

    initial commit

---
+++ src/main/java/kz/teacher/forge/teacherforge/TeacherForgeApplication.java
@@ -0,0 +1,13 @@
+package kz.teacher.forge.teacherforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/teacher/forge/teacherforge/TeacherForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.teacher.forge.teacherforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
